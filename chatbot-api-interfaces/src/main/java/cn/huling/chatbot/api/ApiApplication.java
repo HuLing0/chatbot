2e97b222e0645cffff560bc045cf0d5b732e3ddc
huling <1187728278@qq.com>
2024-08-12 23:45:50 +0800

    proj-init

--- chatbot-api-interfaces/src/main/java/cn/huling/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/cn/huling/chatbot/api/ApiApplication.java
@@ -2,7 +2,7 @@ package cn.huling.chatbot.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class ApiApplication {
 
